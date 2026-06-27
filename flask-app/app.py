fd36db3c647cad9507e268a4064432cb041202fc
vizzz40 <vsarraf70@gmail.com>
2026-06-27 19:29:48 +0200

    refactor some code

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask
-from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
-# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
 
 mysql = MySQL()
 
 mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
 
-# MySQL configurations
+# MySQL config
 app.config['MYSQL_DATABASE_USER'] = 'db_user'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Passw0rd'
 app.config['MYSQL_DATABASE_DB'] = 'employee_db'
